77f7e2412306a0cc7f95e221031b113525e87530
nikhilnk2206 <154123463+nikhilnk2206@users.noreply.github.com>
2026-02-08 14:15:44 +0530

    build fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -11,7 +10,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: path.resolve(__dirname, "../server/dist"),
+    outDir: "dist",
     emptyOutDir: true,
   },
 });
